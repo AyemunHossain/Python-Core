b0c34aa79bc018489bd6b8dd5a8fc3b79ee89a28
Ashik <aharonno2017@gmail.com>
2020-01-31 16:40:08 +0600

    Sum of user input

---
+++ Sum of two user input numbers.py
@@ -0,0 +1,22 @@
+#;==========================================
+#; Title:  Sum of two user input numbers
+#; Author: @AyemunHossain
+#;==========================================
+
+#>>> The simplest one
+#Apporach : 1
+a = input()
+b = input()
+
+sum = a+b
+print(sum)
+
+
+#Apporach : 2
+a = int(input())
+b = int(input())
+
+sum = a+b
+print(sum)
+    
+
