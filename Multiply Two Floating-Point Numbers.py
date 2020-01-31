82f65d25a50546b342e741460eab15e8ae289d8b
Ashik <aharonno2017@gmail.com>
2020-01-31 18:55:46 +0600

    Multiply two numbers

---
+++ Multiply Two Floating-Point Numbers.py
@@ -0,0 +1,14 @@
+#Apporach : 1
+a = 5.66
+b = 9.66
+
+mul = a*b
+print(f"Multiplication of {a} * {b} = {mul}")
+
+#Apporach : 2
+
+a = 5.66
+b = 9.66
+
+mul = a*b
+print(f"Multiplication of {a} * {b} = {round(mul,2)}")
