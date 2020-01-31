c26ac713b85b5205723ca0985d74d9b44f9e6019
Ashik <aharonno2017@gmail.com>
2020-01-31 16:39:16 +0600

    Sum of two numbers

---
+++ Sum of two numbers.py
@@ -0,0 +1,16 @@
+#;==========================================
+#; Title:  Sum of Two numbers
+#; Author: @AyemunHossain
+#;==========================================
+
+
+a = 10  
+b = 5
+sum = a+b
+print(sum)
+
+#or 
+
+a = 10  
+b = 5
+print(a+b)
