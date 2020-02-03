3cef68617b7e311be3ea0b7f4b0258b9e4394c8b
Ashik <aharonno2017@gmail.com>
2020-02-03 13:25:17 +0600

    list added

---
+++ List part 1.py.py
@@ -0,0 +1,22 @@
+#;==========================================
+#; Title:  Python list part 1
+#; Author: @AyemunHossain
+#;==========================================
+
+
+#Simple list
+numbers = [1,2,3,4,5,6,7,8,9]
+alphabets = ['a','b','c','d','e','f']
+
+print(numbers)
+print(alphabets)
+
+#Lets add some items:
+numbers.append(10)
+numbers.append(11)
+
+alphabets.append('g')
+alphabets.append('h')
+
+print(numbers)
+print(alphabets)
